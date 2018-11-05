8993381a624a10ba8dd75249ede283a7c6182d5f
Himehane <370252030@qq.com>
2018-11-05 10:18:37 +0800

    添修改修改警åcontroller内容

--- src/main/java/com/yusys/test/controller/HelloController.java
+++ src/main/java/com/yusys/test/controller/HelloController.java
@@ -4,8 +4,6 @@
 package com.yusys.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,20 +21,9 @@ public class HelloController {
 
 	@Autowired
 	private UserService userService;
-	// 一次性加载定义文件
-	@Autowired
-	private Environment environment;
-	@Value("${name}")
-	private String name;
-	@Value("${url}")
-	private String url;
 
 	@GetMapping("/hello") // == @RequestMapping(path="/hello",method = {RequestMethod.GET} )
 	public String hello() {
-		System.out.println("name=" + environment.getProperty("name"));
-		System.out.println("url=" + environment.getProperty("url"));
-		System.out.println("name" + "\t" + name);
-		System.out.println("url" + "\t" + url);
 		this.userService.say();
 		return "Hello SpringBoot!";
 	}
