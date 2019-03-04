d845082e1ee45a154f00c02a81e9ae6acb71b44e
Himehane <370252030@qq.com>
2019-03-04 12:40:24 +0800

    BaseService层添加mapper常用方法

--- src/main/java/com/springboot/demo/service/BaseEntityService.java
+++ src/main/java/com/springboot/demo/service/BaseEntityService.java
@@ -1,8 +1,26 @@
 package com.springboot.demo.service;
 
+import java.io.Serializable;
 import java.util.List;
 
 public interface BaseEntityService<T> {
+
+    void saveSelective(T entity);
+
+    void updateSelective(T entity);
+
+    void delete(Serializable id);
+
+    void deleteAll(String idField, Serializable[] ids);
+
+    T findOne(Serializable id);
+
+    List<T> findAll();
+
+    List<T> findAllByWhere(T entity);
+
+    int countByWhere(T entity);
+
     /** 分页查询 */
-	List<T> findByPage(Integer pageNum, Integer pageSize);
+    List<T> findByPage(Integer pageNum, Integer pageSize);
 }
--- src/main/java/com/springboot/demo/service/impl/BaseEntityServiceImpl.java
+++ src/main/java/com/springboot/demo/service/impl/BaseEntityServiceImpl.java
@@ -1,6 +1,8 @@
 package com.springboot.demo.service.impl;
 
+import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
+import java.util.Arrays;
 import java.util.List;
 
 import com.github.pagehelper.PageHelper;
@@ -9,6 +11,8 @@ import com.springboot.demo.service.BaseEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.entity.Example;
+import tk.mybatis.mapper.entity.Example.Criteria;
 public abstract class BaseEntityServiceImpl<T> implements BaseEntityService<T>{
 
     @Autowired
@@ -25,6 +29,36 @@ public abstract class BaseEntityServiceImpl<T> implements BaseEntityService<T>{
         /* 获取所有的实际类型参数，取第一个*/
 		this.entityClass = (Class<T>)parameterizedType.getActualTypeArguments()[0];
     }
+
+	public void saveSelective(T entity){
+		mapper.insertSelective(entity);
+	}
+	public void updateSelective(T entity){
+		mapper.updateByPrimaryKeySelective(entity);
+	}
+	public void delete(Serializable id){
+		mapper.deleteByPrimaryKey(id);
+	}
+	public void deleteAll(String idField, Serializable[] ids){
+		Example example = new Example(entityClass);
+		Criteria criteria = example.createCriteria();
+		criteria.andIn(idField, Arrays.asList(ids));
+		mapper.deleteByExample(example);
+	}
+	public T findOne(Serializable id){
+		return mapper.selectByPrimaryKey(id);
+	}
+	public List<T> findAll(){
+		return mapper.selectAll();
+	}
+	
+	
+	public int countByWhere(T entity){
+		return mapper.selectCount(entity);
+	}
+	public List<T> findAllByWhere(T entity){
+		return mapper.select(entity);
+	}
 
     /** 分页查询 */
 	public List<T> findByPage(Integer pageNum, Integer pageSize){
